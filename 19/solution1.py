bb575491ff9ed2824726185f2efedccb14a0f67a
AdamBalski <adambalski1@gmail.com>
2024-12-19 07:35:44 +0100

    upsolve day 19 wrt. runtime

--- 19/solution1.py
+++ 19/solution1.py
@@ -1,18 +1,21 @@
 import sys
 
-avds = next(sys.stdin).rstrip().split(", ")
+avds = set(next(sys.stdin).rstrip().split(", "))
 next(sys.stdin)
 designs = [line.rstrip() for line in sys.stdin]
+max_avd_len = max(map(len, avds))
 
 def can_design(design):
     possible = {0}
     for i in range(1, len(design) + 1):
-        for avd in avds:
-            if design[i - len(avd):i] != avd:
+        for avd_len in range(1, min(i, max_avd_len) + 1):
+            if i - avd_len not in possible:
                 continue
-            if i - len(avd) not in possible:
+            avd_candidate = design[i - avd_len:i]
+            if avd_candidate not in avds:
                 continue
             possible.add(i)
+            break
     return len(design) in possible
 
 result = 0
--- 19/solution2.py
+++ 19/solution2.py
@@ -1,19 +1,21 @@
 import sys
 import collections
 
-avds = next(sys.stdin).rstrip().split(", ")
+avds = set(next(sys.stdin).rstrip().split(", "))
 next(sys.stdin)
 designs = [line.rstrip() for line in sys.stdin]
+max_avd_len = max(map(len, avds))
 
 def count_ways(design):
     ways = collections.defaultdict(int)
     ways[0] = 1
 
     for i in range(1, len(design) + 1):
-        for avd in avds:
-            if design[i - len(avd):i] != avd:
+        for avd_len in range(1, min(i, max_avd_len) + 1):
+            avd_candidate = design[i - avd_len:i]
+            if avd_candidate not in avds:
                 continue
-            ways[i] += ways[i - len(avd)]
+            ways[i] += ways[i - avd_len]
     return ways[len(design)]
 
 
