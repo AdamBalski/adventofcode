ea4eecaefea0ac292419b9cfeeaa1e1fe7ee1e71
AdamBalski <adambalski1@gmail.com>
2024-12-23 15:37:26 +0100

    remove unused union find, day 23/part 1

--- 23/solution1.py
+++ 23/solution1.py
@@ -2,51 +2,6 @@ import sys
 sys.path.append('/'.join(__file__.split("/")[:-2]))
 import utils
 
-class UnionFind:
-    def __init__(self):
-        self.roots = {}
-
-    def __len__(self):
-        return len(self.roots)
-
-    def __contains__(self, x):
-        return x in self.roots
-
-    def find(self, p):
-        if p not in self.roots:
-            self.roots[p] = p
-            return p
-        while self.roots[p] != p:
-            p, self.roots[p] = self.roots[p], self.roots[self.roots[p]]
-        return p
-
-    def union(self, p, q):
-        p = self.find(p)
-        q = self.find(q)
-        self.roots[p] = q
-
-    def connected(self, p, q):
-        return self.find(p) == self.find(q)
-
-    def subset(self, p):
-        # I guess each parent could have linked lists with head and tail pointers instead
-        res = []
-        for num in self.roots:
-            if self.connected(p, num):
-                res.append(num)
-        return res
-
-    def subsets(self):
-        seen = set()
-        for node in self.roots:
-            if node in seen:
-                continue
-            curr = self.subset(node)
-            for c in curr:
-                seen.add(c)
-            yield curr
-
-uf = UnionFind()
 comps = []
 conns = set()
 for c1, c2 in list(utils.input_lines("{}-{}")):
@@ -54,7 +9,6 @@ for c1, c2 in list(utils.input_lines("{}-{}")):
     conns.add((c2, c1))
     comps.append(c1)
     comps.append(c2)
-    uf.union(c1, c2)
 comps = list(set(comps))
 
 cnter = 0
